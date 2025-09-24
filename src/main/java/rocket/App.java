745b7a23a10dd1e54c598db32e815d12b1f5f965
v srihari <vsriharisrihari9@gmail.com>
2025-09-24 14:46:29 +0530

    Update App.java

--- src/main/java/rocket/App.java
+++ src/main/java/rocket/App.java
@@ -6,6 +6,7 @@ package rocket;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
          
     }
 }
